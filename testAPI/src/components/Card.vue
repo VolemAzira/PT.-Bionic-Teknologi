f4bebc5c5fa705f0eef87206309274f71486e9df
VolemAzira <volemalvaroazira2@gmail.com>
2023-10-05 11:16:38 +0700

    API

---
+++ testAPI/src/components/Card.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: "Judul",
+  },
+});
+</script>
+
+<template>
+  <div
+    class="bg-primary flex items-center justify-between p-3 px-5 rounded-lg shadow-lg mx-auto"
+  >
+    <h1 class="text-green-600 font-semibold">{{ props.title }}</h1>
+    <button class="bg-orange-500 px-4 py-1 rounded-lg text-white font-semibold">
+      See Job
+    </button>
+  </div>
+</template>
---
+++ testAPI/src/pages/Home.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Card from "../components/Card.vue";
+
+const URL_API = "https://bti.id/services/btiportalcorems/api/pt-job-posts/no-auth";
+let cardData = [];
+
+// Mengambil data dari API
+async function fetchData() {
+  try {
+    const response = await fetch(URL_API);
+    const data = await response.json();
+    cardData = data; // Menyimpan data ke variabel cardData
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+fetchData(); 
+</script>
+
+<template>
+  <main class="mx-10">
+    <h1>Home</h1>
+    <div>
+      <!-- Mengulang data dari API untuk membuat Card -->
+      <Card v-for="(item, index) in cardData" :key="index" :title="item.title" />
+    </div>
+  </main>
+</template>
